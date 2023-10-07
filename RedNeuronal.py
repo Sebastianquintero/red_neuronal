c8866d119452581137f0d1216a47f75a183859f3
Sebas <sebastquinter87@gmail.com>
2023-10-06 23:41:28 -0500

    Red_Neuronal1.0

---
+++ RedNeuronal.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+#-------------Declaramos los eventos pasados -----------
+
+entrada = np.array([1, 6, 30, 7, 70, 43, 503, 201, 1005, 99], dtype=float)
+
+
+
